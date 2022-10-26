188637ecb1cb581499c47aeef1df752dd4fb6554
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-10-26 14:25:41 -0600

    Updated gitignore file

---
+++ src/main/java/com/exercise/wordlelettercount/WordleLetterCountApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.wordlelettercount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleLetterCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleLetterCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/wordlelettercount/WordleLetterCountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.wordlelettercount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleLetterCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
